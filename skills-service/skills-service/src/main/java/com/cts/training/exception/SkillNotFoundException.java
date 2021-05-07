b4ad89270bdc79bc345d350700c20fe974cf29e5
Sreevas91 <80826906+Sreevas91@users.noreply.github.com>
2021-05-07 22:57:23 +0530

    Add files via upload

---
+++ skills-service/skills-service/src/main/java/com/cts/training/exception/SkillNotFoundException.java
@@ -0,0 +1,13 @@
+package com.cts.training.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@SuppressWarnings("serial")
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+public class SkillNotFoundException extends RuntimeException {
+
+	public SkillNotFoundException(String message) {
+		super(message);
+	}
+}
---
+++ skills-service/skills-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into skill (user_id,name,description,level) values(1001L, 'Abid','Java','Beginner');
+insert into skill (user_id,name,description,level) values(1002L, 'Lasya','Ad_java','Intermediate');
+insert into skill (user_id,name,description,level) values(1003L, 'Billa','CSS','Beginner');
+insert into skill (user_id,name,description,level) values(1004L, 'Lakshmi','Spring MVC','Expert');
+insert into skill (user_id,name,description,level) values(1005L, 'Vamsi','Spring Boot','Intermediate');
